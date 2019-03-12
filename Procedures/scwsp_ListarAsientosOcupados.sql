0e8b673d2eca8f147838f72269fb4fadd47d577d
777sc <46906763+777sc@users.noreply.github.com>
2019-03-11 19:46:26 -0500

    Update scwsp_ListarAsientosOcupados.sql

--- Procedures/scwsp_ListarAsientosOcupados.sql
+++ Procedures/scwsp_ListarAsientosOcupados.sql
@@ -1,4 +1,4 @@
-ALter Procedure scwsp_ListarAsientosOcupados
+ALTER Procedure scwsp_ListarAsientosOcupados 
 @Codi_Programacion		Int,
 @Fecha_Programacion		SmallDateTime,
 @Nro_Viaje				Int,
@@ -13,7 +13,7 @@ as
 			Select
 			NUME_ASIENTO, 0 Tipo_Documento,'' Numero_Documento,'' Ruc_Contacto,
 			'01/01/1990' Fecha_Viaje,'01/01/1990' Fecha_Venta,'' Nacionalidad,0 Precio_Venta, '' Recoge_En,
-			0 Color,'AB' FLAG_VENTA,'' Sigla, 0 Codi_Origen, 0 Codi_Destino	 From ASIENTO 
+			0 Color,'AB' FLAG_VENTA,'' Sigla, 0 Codi_Origen, 0 Codi_Destino,''  Boleto,''tipo	 From ASIENTO 
 			Where CODI_PROGRAMACION=@Nro_Viaje and t_ruta='V' and fecha=@Fecha_Programacion
 		End
 	Else
@@ -22,7 +22,7 @@ as
 			--Tabla Venta
 			Select v.NUME_ASIENTO,v.TIPO_DOC Tipo_Documento,v.DNI Numero_Documento,v.NIT_CLIENTE Ruc_Contacto,
 			vd.Fecha_Viaje,v.FECH_VENTA Fecha_Venta,vd.Nacionalidad,v.PREC_VENTA Precio_Venta,v.RECO_VENTA Recoge_En,
-			cd.Color, FLAG_VENTA,pv.Sigla, v.cod_origen Codi_Origen, v.CODI_SUBRUTA Codi_Destino From Venta v
+			cd.Color, FLAG_VENTA,pv.Sigla, v.cod_origen Codi_Origen, v.CODI_SUBRUTA Codi_Destino,vd.NUME_BOLETO Boleto,v.tipo From Venta v
 			Inner Join VENTA_DERIVADA vd on v.id_venta=vd.id_venta
 			Inner Join Tb_Colores_Destino cd on v.CODI_SUBRUTA=cd.Codi_destino and vd.Servicio=cd.codi_Servicio
 			Inner Join Tb_PuntoVenta pv on v.Punto_Venta=pv.Codi_puntoVenta
@@ -35,7 +35,7 @@ as
 			Select
 			NUME_ASIENTO, 0 Tipo_Documento,'' Numero_Documento,'' Ruc_Contacto,
 			'01/01/1990' Fecha_Viaje,'01/01/1990' Fecha_Venta,'' Nacionalidad,0 Precio_Venta, '' Recoge_En,
-			0 Color,'' FLAG_VENTA,'' Sigla, 0 Codi_Origen, 0 Codi_Destino From ASIENTO 
+			0 Color,'' FLAG_VENTA,'' Sigla, 0 Codi_Origen, 0 Codi_Destino,''  Boleto,''tipo From ASIENTO 
 			Where CODI_PROGRAMACION=@Codi_Programacion and t_ruta='P'
 			Union
 			
@@ -43,7 +43,9 @@ as
 			Select
 			NUME_ASIENTO, 0 Tipo_Documento,'' Numero_Documento,'' Ruc_Contacto,
 			'01/01/1990' Fecha_Viaje,'01/01/1990' Fecha_Venta,'' Nacionalidad,0 Precio_Venta, '' Recoge_En,
-			0 Color,'' FLAG_VENTA,'' Sigla, 0 Codi_Origen, 0 Codi_Destino From ASIENTO 
+			0 Color,'' FLAG_VENTA,'' Sigla, 0 Codi_Origen, 0 Codi_Destino,''  Boleto,''tipo From ASIENTO 
 			Where CODI_PROGRAMACION=@Nro_Viaje and t_ruta='V' and fecha=@Fecha_Programacion
 		End
+
+
 
