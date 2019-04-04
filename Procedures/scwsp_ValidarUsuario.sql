2feacf34aa2c4ad55ee6a774f7a054df2bf7d81c
777sc <46906763+777sc@users.noreply.github.com>
2019-04-04 16:33:40 -0500

    Update scwsp_ValidarUsuario.sql

--- Procedures/scwsp_ValidarUsuario.sql
+++ Procedures/scwsp_ValidarUsuario.sql
@@ -1,14 +1,23 @@
-Create Procedure scwsp_ValidarUsuario
-@Codi_Usuario		SmallInt,
-@Pws				Varchar(30)
-as
-	Select 
-	Codi_Usuario	,
-	Codi_Empresa	,
-	Codi_Sucursal	,
-	Codi_puntoVenta	,
-	Pws				,
-	Nivel 
-	From 
-	Tb_Usuario
-	Where Codi_Usuario=@Codi_Usuario and Pws=@Pws
+ALTER PROCEDURE scwsp_ValidarUsuario
+@Codi_Usuario		SmallInt
+AS
+	SELECT
+		U.Codi_Usuario	,
+		U.Login,
+		U.Codi_Empresa	,
+		U.Codi_Sucursal	,
+		U.Codi_puntoVenta	,
+		U.Pws				,
+		U.Nivel,
+		O.Descripcion Nom_Sucursal,
+		PV.Descripcion Nom_PuntoVenta,
+		U.Terminal
+	FROM 
+		Tb_Usuario U
+		LEFT JOIN Tb_Oficinas O
+			ON U.Codi_Sucursal = O.Codi_Sucursal
+		LEFT JOIN Tb_PuntoVenta PV
+			ON U.Codi_Sucursal =PV.Codi_Sucursal
+			AND U.Codi_puntoVenta = PV.Codi_puntoVenta
+	WHERE
+		Codi_Usuario = @Codi_Usuario
