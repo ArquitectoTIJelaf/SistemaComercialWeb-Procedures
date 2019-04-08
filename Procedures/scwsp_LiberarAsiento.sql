b2cba5f9543d05adf750edc5a3e784f817e66629
777sc <46906763+777sc@users.noreply.github.com>
2019-04-08 16:28:05 -0500

    Update scwsp_LiberarAsiento.sql

--- Procedures/scwsp_LiberarAsiento.sql
+++ Procedures/scwsp_LiberarAsiento.sql
@@ -1,11 +1,9 @@
-Create procedure scwsp_LiberarAsiento
+Alter procedure scwsp_LiberarAsiento
 @Ids		Int
-as
-  Begin Transaction  
-		delete from ASIENTO where IDS =@Ids
- If @@Error<>0
-        RollBack Transaction                
-    Else
-       Commit TRANSACTION
-
-
+As
+	Begin Transaction  
+		Delete from ASIENTO where IDS =@Ids
+	If @@Error<>0
+		RollBack Transaction                
+	Else
+		Commit Transaction
