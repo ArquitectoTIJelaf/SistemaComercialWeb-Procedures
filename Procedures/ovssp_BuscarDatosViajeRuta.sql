97966d0582c7988051ba0f82d7e983e1ff26a116
Williams Morales Caballero <williams.morales.caballero@gmail.com>
2019-04-25 12:54:58 -0500

    Elimino Procedures Incorrectos

--- Procedures/ovssp_BuscarDatosViajeRuta.sql
+++
@@ -1,7 +0,0 @@
-Create Procedure ovssp_BuscarDatosViajeRuta
-@Nro_Viaje			Int
-as
-select distinct rm.CODI_SUCURSAL,rm.CODI_DESTINO Codi_Ruta,
-rm.CODI_SERVICIO,mp.CODI_EMPRESA from Tb_Maestro_Programacion mp
-Inner Join Tb_Ruta_Maestro rm on mp.NRO_RUTA=rm.NRO_RUTA
-where mp.NRO_VIAJE=@Nro_Viaje
