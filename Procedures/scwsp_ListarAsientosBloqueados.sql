a1cdbc9aeadfd245fb9dec186774ec197e468b97
777sc <46906763+777sc@users.noreply.github.com>
2019-04-26 14:46:05 -0500

    Update scwsp_ListarAsientosBloqueados.sql

--- Procedures/scwsp_ListarAsientosBloqueados.sql
+++ Procedures/scwsp_ListarAsientosBloqueados.sql
@@ -1,6 +1,8 @@
-ALter Procedure scwsp_ListarAsientosBloqueados
-@Nro_Viaje				Int
-as
+Alter Procedure scwsp_ListarAsientosBloqueados
+@Nro_Viaje				Int,
+@Codi_Programacion		Int,
+@Fecha_Programacion		SmallDateTime
+As
 	Declare @Codi_Sucursal			SmallInt
 	Declare @Codi_Ruta				SmallInt
 	Declare @Codi_Servicio			TinyInt
@@ -8,11 +10,17 @@ as
 	Declare @Turno					Varchar(10)
 
 
-	Select @Codi_Empresa= mp.Codi_Empresa,@Codi_Sucursal=rm.Codi_Sucursal,
-	@Codi_Ruta=rm.CODI_DESTINO,@Codi_Servicio=rm.Codi_Servicio,@Turno=mp.HORA 
-	From Tb_Maestro_Programacion mp
-	Inner Join Tb_Ruta_Maestro rm on mp.NRO_RUTA=rm.NRO_RUTA
-	Where mp.NRO_VIAJE=@Nro_Viaje
+	Select
+		@Codi_Empresa = mp.Codi_Empresa,
+		@Codi_Sucursal = rm.Codi_Sucursal,
+		@Codi_Ruta = rm.CODI_DESTINO,
+		@Codi_Servicio = rm.Codi_Servicio,
+		@Turno = mp.HORA 
+	From
+		Tb_Maestro_Programacion mp
+		Inner Join Tb_Ruta_Maestro rm on mp.NRO_RUTA=rm.NRO_RUTA
+	Where
+		mp.NRO_VIAJE=@Nro_Viaje
 
 	Declare @Asientos				Varchar(100)
 	Declare @Codi_OrigenP			SmallInt
@@ -23,27 +31,81 @@ as
 	Declare @Tabla					Table (Nume_Asiento Int, Codi_Origen SmallInt, Codi_Destino SmallInt)
 
 	DECLARE AsientoBloqInfo CURSOR FOR 
-				Select
-				Asientos,cod_OrigenP,Cod_DestinoP From Tb_AsientosBloqueados 
-				Where Cod_OrigenB=@Codi_Sucursal and Cod_DestinoB=@Codi_Ruta 
-				and Cod_Servicio=@Codi_Servicio and cod_empresa=@Codi_Empresa
-				and horario=@Turno
+		Select
+			Asientos,cod_OrigenP,Cod_DestinoP
+		From
+			Tb_AsientosBloqueados 
+		Where
+			Cod_OrigenB=@Codi_Sucursal
+			and Cod_DestinoB=@Codi_Ruta 
+			and Cod_Servicio=@Codi_Servicio
+			and cod_empresa=@Codi_Empresa
+			and horario=@Turno
 	OPEN AsientoBloqInfo
-	FETCH NEXT FROM AsientoBloqInfo INTO @Asientos,@Codi_OrigenP,@Codi_DestinoP
+	FETCH NEXT FROM AsientoBloqInfo INTO @Asientos, @Codi_OrigenP, @Codi_DestinoP
 	WHILE @@fetch_status = 0
 	BEGIN
-		Set @CantBloques=Len(@Asientos)
-		Set @NumeroBloque=1
-		While (@NumeroBloque<@CantBloques)
+		Set @CantBloques = Len(@Asientos)
+		Set @NumeroBloque = 1
+		While (@NumeroBloque < @CantBloques)
 			Begin
-				Insert Into @Tabla(Nume_Asiento,Codi_Origen,Codi_Destino)
-				Values(cast(SUBSTRING(@Asientos,@NumeroBloque,2)as Int),@Codi_OrigenP,@Codi_DestinoP)
-				Set @NumeroBloque=@NumeroBloque+2
+				Insert Into @Tabla(Nume_Asiento, Codi_Origen, Codi_Destino)
+				Values(cast(SUBSTRING(@Asientos, @NumeroBloque, 2)as Int),@Codi_OrigenP, @Codi_DestinoP)
+				Set @NumeroBloque = @NumeroBloque + 2
 			End
-		FETCH NEXT FROM AsientoBloqInfo INTO @Asientos,@Codi_OrigenP,@Codi_DestinoP
+		FETCH NEXT FROM AsientoBloqInfo INTO @Asientos, @Codi_OrigenP, @Codi_DestinoP
 	END
 	CLOSE AsientoBloqInfo
 	DEALLOCATE AsientoBloqInfo
 
-	Select
-	Nume_Asiento, Codi_Origen, Codi_Destino From @Tabla 
+	If @Codi_Programacion = 0
+		Begin
+			-- Tb_AsientosBloqueados
+			Select
+				Nume_Asiento, Codi_Origen, Codi_Destino
+			From
+				@Tabla
+			
+			UNION
+
+			-- Tabla Asiento con Nro Viaje
+			Select
+				NUME_ASIENTO, 0 Codi_Origen, 0 Codi_Destino
+			From
+				ASIENTO 
+			Where
+				CODI_PROGRAMACION=@Nro_Viaje
+				and t_ruta='V'
+				and fecha=@Fecha_Programacion
+		End
+	Else
+		Begin
+			-- Tb_AsientosBloqueados
+			Select
+				Nume_Asiento, Codi_Origen, Codi_Destino
+			From
+				@Tabla
+
+			UNION
+
+			-- Tabla Asiento con Nro Viaje
+			Select
+				NUME_ASIENTO, 0 Codi_Origen, 0 Codi_Destino
+			From
+				ASIENTO 
+			Where
+				CODI_PROGRAMACION=@Nro_Viaje
+				and t_ruta='V'
+				and fecha=@Fecha_Programacion
+
+			UNION
+
+			-- Tabla Asiento con Programacion
+			Select
+				NUME_ASIENTO, 0 Codi_Origen, 0 Codi_Destino
+			From
+				ASIENTO
+			Where
+				CODI_PROGRAMACION = @Codi_Programacion
+				and t_ruta='P'
+		End
