d288a9bd4742c295a77cc4308155c8762c1ee3f6
777sc <46906763+777sc@users.noreply.github.com>
2019-04-29 11:03:27 -0500

    Update scwsp_ValidarBloqueoAsiento.sql

--- Procedures/scwsp_ValidarBloqueoAsiento.sql
+++ Procedures/scwsp_ValidarBloqueoAsiento.sql
@@ -5,56 +5,102 @@ Alter Procedure scwsp_ValidarBloqueoAsiento
 @Codi_Destino			SmallInt,
 @Nume_Asiento			Varchar(2),
 @Fecha_Programacion		SmallDatetime
-as
+As
 
 Set NoCount On
+	
+	-- Tabla Tb_AsientosBloqueados
+	Declare @Codi_Sucursal			SmallInt = 0;
+	Declare @Codi_Ruta				SmallInt = 0;
+	Declare @Codi_Servicio			TinyInt = 0;
+	Declare @Codi_Empresa			TinyInt = 0;
+	Declare @Turno					Varchar(10) = '';
 
-	Declare @Codi_Sucursal			SmallInt
-	Declare @Codi_Ruta				SmallInt
-	Declare @Codi_Servicio			TinyInt
-	Declare @Codi_Empresa			TinyInt
-	Declare @Turno					Varchar(10)
+	Select 
+		@Codi_Empresa= mp.Codi_Empresa,
+		@Codi_Sucursal=rm.Codi_Sucursal,
+		@Codi_Ruta=rm.CODI_DESTINO,
+		@Codi_Servicio=rm.Codi_Servicio,
+		@Turno=mp.HORA 
+	From
+		Tb_Maestro_Programacion mp
+		Inner Join Tb_Ruta_Maestro rm
+		on mp.NRO_RUTA=rm.NRO_RUTA
+	Where
+		mp.NRO_VIAJE=@Nro_Viaje
+	
+	Declare @Asientos				Varchar(100)
+	Declare @Codi_OrigenP			SmallInt
+	Declare @Codi_DestinoP			SmallInt
+	
+	Declare @NumeroBloque			Int
+	Declare @CantBloques			Int
+	Declare @Tabla					Table (Nume_Asiento Int, Codi_Origen SmallInt, Codi_Destino SmallInt)
+	
+	DECLARE AsientoBloqInfo CURSOR FOR 
+		Select
+			Asientos,cod_OrigenP,Cod_DestinoP
+		From
+			Tb_AsientosBloqueados 
+		Where
+			Cod_OrigenB=@Codi_Sucursal
+			and Cod_DestinoB=@Codi_Ruta 
+			and Cod_Servicio=@Codi_Servicio
+			and cod_empresa=@Codi_Empresa
+			and horario=@Turno
+	OPEN AsientoBloqInfo
+	FETCH NEXT FROM AsientoBloqInfo INTO @Asientos, @Codi_OrigenP, @Codi_DestinoP
+	WHILE @@fetch_status = 0
+	BEGIN
+		Set @CantBloques = Len(@Asientos)
+		Set @NumeroBloque = 1
+		While (@NumeroBloque < @CantBloques)
+			Begin
+				Insert Into @Tabla(Nume_Asiento, Codi_Origen, Codi_Destino)
+				Values(cast(SUBSTRING(@Asientos, @NumeroBloque, 2)as Int),@Codi_OrigenP, @Codi_DestinoP)
+				Set @NumeroBloque = @NumeroBloque + 2
+			End
+		FETCH NEXT FROM AsientoBloqInfo INTO @Asientos, @Codi_OrigenP, @Codi_DestinoP
+	END
+	CLOSE AsientoBloqInfo
+	DEALLOCATE AsientoBloqInfo
 
-	Set @Codi_Sucursal	= 0
-	Set @Codi_Ruta		= 0
-	Set @Codi_Servicio	= 0
-	Set @Codi_Empresa	= 0
-	Set @Turno			= ''
-
-	Select @Codi_Empresa= mp.Codi_Empresa,@Codi_Sucursal=rm.Codi_Sucursal,
-	@Codi_Ruta=rm.CODI_DESTINO,@Codi_Servicio=rm.Codi_Servicio,@Turno=mp.HORA 
-	From Tb_Maestro_Programacion mp
-	Inner Join Tb_Ruta_Maestro rm on mp.NRO_RUTA=rm.NRO_RUTA
-	Where mp.NRO_VIAJE=@Nro_Viaje
-
-	If @Codi_Programacion>0
+	If @Codi_Programacion > 0
 		Begin
 			SELECT Top 1 1 FROM Asiento 
-			where CODI_PROGRAMACION=@Codi_Programacion AND NUME_ASIENTO=@Nume_Asiento and t_ruta='P'
-			Union
+			where
+				CODI_PROGRAMACION=@Codi_Programacion
+				and NUME_ASIENTO=@Nume_Asiento
+				and t_ruta='P'
+
+			UNION
+
 			SELECT Top 1 1 FROM VENTA 
-			where CODI_PROGRAMACION=@Codi_Programacion AND NUME_ASIENTO=@Nume_Asiento 
-			and cod_origen=@Codi_Origen and CODI_SUBRUTA=@Codi_Destino
-			and INDI_ANULADO='F'
-			--Union
-			--Select Top 1 1 From Tb_AsientosBloqueados 
-			--Where cod_OrigenP=@Codi_Origen and Cod_DestinoP=@Codi_Destino
-			--and Cod_OrigenB=@Codi_Sucursal and Cod_DestinoB=@Codi_Ruta 
-			--and Cod_Servicio=@Codi_Servicio and cod_empresa=@Codi_Empresa
-			--and horario=@Turno AND NUME_ASIENTO=@Nume_Asiento
+			where
+				CODI_PROGRAMACION=@Codi_Programacion
+				and NUME_ASIENTO=@Nume_Asiento 
+				and cod_origen=@Codi_Origen
+				and CODI_SUBRUTA=@Codi_Destino
+				and INDI_ANULADO='F'
+
+			UNION
+
+			SELECT Top 1 1 FROM @Tabla
+			where
+				NUME_ASIENTO= Cast(@Nume_Asiento as int)
 		End
 	Else
 		Begin
 			SELECT Top 1 1 FROM Asiento 
-			where CODI_PROGRAMACION=@Nro_Viaje AND NUME_ASIENTO=@Nume_Asiento and t_ruta='V'
-			and Fecha=@Fecha_Programacion
-			--Union
-			--Select Top 1 1 From Tb_AsientosBloqueados 
-			--Where cod_OrigenP=@Codi_Origen and Cod_DestinoP=@Codi_Destino
-			--and Cod_OrigenB=@Codi_Sucursal and Cod_DestinoB=@Codi_Ruta 
-			--and Cod_Servicio=@Codi_Servicio and cod_empresa=@Codi_Empresa
-			--and horario=@Turno
+			where
+				CODI_PROGRAMACION=@Nro_Viaje
+				and NUME_ASIENTO=@Nume_Asiento
+				and t_ruta='V'
+				and Fecha=@Fecha_Programacion
+
+			UNION
+
+			SELECT Top 1 1 FROM @Tabla
+			where
+				NUME_ASIENTO= Cast(@Nume_Asiento as int)
 		End
-	
-
-
