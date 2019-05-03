4ae0267fab037308ece340e07bd403d987d060bb
777sc <46906763+777sc@users.noreply.github.com>
2019-05-03 14:42:17 -0500

    Update scwsp_GrabarAuditoria.sql

--- Procedures/scwsp_GrabarAuditoria.sql
+++ Procedures/scwsp_GrabarAuditoria.sql
@@ -1,52 +1,53 @@
-Create Procedure scwsp_GrabarAuditoria 
-@Codi_Usuario		SmallInt,  
-@Nom_Usuario		Varchar(50),  
-@Tabla				Varchar(30),  
-@Tipo_Movimiento	Varchar(30),  
-@Boleto				Varchar(15),  
-@Nume_Asiento		Varchar(10),  
-@Nom_Oficina		Varchar(30),  
-@Nom_PuntoVenta		Varchar(30),  
-@Pasajero			Varchar(50),  
-@Fecha_Viaje		DateTime,  
-@Hora_Viaje			Varchar(30),  
-@Nom_Destino		Varchar(30),  
-@Precio				Real,  
-@Obs1				Varchar(100),  
-@Obs2				Varchar(100),  
-@Obs3				Varchar(100),  
-@Obs4				Varchar(100),  
-@Obs5				Varchar(100)  
-as  
-set nocount on  
-begin  
-	insert into auditoria   
+Alter Procedure scwsp_GrabarAuditoria
+@Codi_Usuario		SmallInt,
+@Nom_Usuario		Varchar(50),
+@Tabla				Varchar(40),
+@Tipo_Movimiento	Varchar(60),
+@Boleto				Varchar(20),
+@Nume_Asiento		Varchar(4),
+@Nom_Oficina		Varchar(40),
+@Nom_PuntoVenta		Varchar(40),
+@Pasajero			Varchar(40),
+@Fecha_Viaje		DateTime,
+@Hora_Viaje			Varchar(30),
+@Nom_Destino		Varchar(40),
+@Precio				Real,
+@Obs1				Varchar(200),
+@Obs2				Varchar(200),
+@Obs3				Varchar(200),
+@Obs4				Varchar(200),
+@Obs5				Varchar(200)
+As
+Set nocount on
+
+Begin
+	insert into auditoria
 		(
-			fecha				, 
-			hora				, 
-			codi_usuario		, 
-			nom_usuario			, 
-			tabla				, 
-			tipo_mov			, 
-			boleto				, 
-			asiento				, 
-			sucursal			, 
-			puntoventa			, 
-			pasajero			, 
-			fecha_viaje			, 
-			hora_viaje			, 
-			destino_pasajero	, 
-			precio				, 
-			obs1				, 
-			obs2				, 
-			obs3				, 
-			obs4				, 
+			fecha				,
+			hora				,
+			codi_usuario		,
+			nom_usuario			,
+			tabla				,
+			tipo_mov			,
+			boleto				,
+			asiento				,
+			sucursal			,
+			puntoventa			,
+			pasajero			,
+			fecha_viaje			,
+			hora_viaje			,
+			destino_pasajero	,
+			precio				,
+			obs1				,
+			obs2				,
+			obs3				,
+			obs4				,
 			obs5
-		)  
-	values  
+		)
+	values
 	(
-			Convert(Varchar(10),getdate(),103)		,
-			Convert(Varchar(10),getdate(),108)		,
+			Convert(Varchar(10),getdate(),103),
+			Convert(Varchar(10),getdate(),108),
 			@Codi_Usuario		,
 			@Nom_Usuario		,
 			@Tabla				,
@@ -65,5 +66,5 @@ begin
 			@Obs3				,
 			@Obs4				,
 			@Obs5
-	)  
+	)
 end
