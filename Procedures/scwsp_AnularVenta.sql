adab41ede3e379528bdb2156f92637f8b2c6d0cf
777sc <46906763+777sc@users.noreply.github.com>
2019-05-10 16:39:30 -0500

    Update scwsp_AnularVenta.sql

--- Procedures/scwsp_AnularVenta.sql
+++ Procedures/scwsp_AnularVenta.sql
@@ -1,9 +1,15 @@
-Alter Procedure scwsp_AnularVenta
-@Id_Venta		Int,--Codigo de Ventas
-@Codi_Usuario	SmallInt--Codigo de Usario de Anulacion
-as
-	Update Venta
-	Set INDI_ANULADO='T',
-		FECH_ANULACION=Convert(Varchar(10),getdate(),103),
-		clav_usuario1=@Codi_Usuario
-	Where Id_Venta=@Id_Venta
+ALTER PROCEDURE scwsp_AnularVenta (
+	@Id_Venta INT, -- Codigo de Ventas
+	@Codi_Usuario SMALLINT -- Codigo de Usario de Anulacion
+)
+AS
+
+UPDATE Venta
+SET INDI_ANULADO = 'T'
+	,FECH_ANULACION = Convert(VARCHAR(10), getdate(), 103)
+	,clav_usuario1 = @Codi_Usuario
+WHERE
+	Id_Venta = @Id_Venta
+	and INDI_ANULADO <> 'T'
+
+SELECT @@ROWCOUNT AS Validator;
