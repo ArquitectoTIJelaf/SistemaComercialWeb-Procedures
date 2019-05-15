ed28f235cc7a6b094f2bc3a11dcc0cdd640d78fa
777sc <46906763+777sc@users.noreply.github.com>
2019-05-15 14:41:45 -0500

    Update scwsp_ValidarSaldoPaseCortesia.sql

--- Procedures/scwsp_ValidarSaldoPaseCortesia.sql
+++ Procedures/scwsp_ValidarSaldoPaseCortesia.sql
@@ -1,9 +1,9 @@
-Create Procedure scwsp_ValidarSaldoPaseCortesia
+Alter Procedure scwsp_ValidarSaldoPaseCortesia
 @Codi_Socio		Varchar(2),
 @Mes			Varchar(2),
 @Anno			Varchar(4)
 as
-	Select top 1 1 From boletos_x_socio 
+	Select top 1 saldo From boletos_x_socio 
 	Where cod_socio=@Codi_Socio
 	and mes=@Mes
 	and anno=@Anno
