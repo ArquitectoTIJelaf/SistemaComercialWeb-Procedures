832c9db9adfa5102e15db0509a07c38631cb3816
777sc <46906763+777sc@users.noreply.github.com>
2019-05-24 14:18:11 -0500

    Update scwsp_BuscarVentaxId.sql

--- Procedures/scwsp_BuscarVentaxId.sql
+++ Procedures/scwsp_BuscarVentaxId.sql
@@ -6,15 +6,17 @@ SELECT
 	,v.PREC_VENTA AS Precio_Venta
 	,p.Codi_ruta
 	,vd.Fecha_Viaje
-	--,vd.NUME_BOLETO
 	,v.SERIE_BOLETO
 	,v.NUME_BOLETO
 	,v.FECH_VENTA AS Fecha_Venta
 	,v.Tipo
+	,c.idtabla -- Para Crédito: IdPrecio
 FROM
 	VENTA v
 	INNER JOIN VENTA_DERIVADA vd ON v.id_venta = vd.id_venta
 	INNER JOIN Tb_Programacion p ON v.CODI_PROGRAMACION = p.Codi_Programacion
+	LEFT JOIN Tb_BoletoxContrato bc ON v.id_venta = bc.Id_Venta
+	LEFT JOIN tb_Control c ON bc.IdRegistro = c.Idboletocontrato
 WHERE
 	v.id_venta = @Id_venta
 	AND v.INDI_ANULADO = 'F'
