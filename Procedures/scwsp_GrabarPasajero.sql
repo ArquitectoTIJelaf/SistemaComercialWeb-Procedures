c340d13859ab426be71e74c512abb8679840809c
777sc <46906763+777sc@users.noreply.github.com>
2019-05-29 18:03:29 -0500

    Update scwsp_GrabarPasajero.sql

--- Procedures/scwsp_GrabarPasajero.sql
+++ Procedures/scwsp_GrabarPasajero.sql
@@ -1,47 +1,55 @@
-Create Procedure scwsp_GrabarPasajero
-@Tipo_Doc_Id		Char(1)			,
-@Numero_Doc			Varchar(15)		,
-@Nombre_Clientes	Varchar(100)	,
-@Apellido_P			Varchar(50)		,
-@Apellido_M			Varchar(50)		,
-@fec_nac			SmallDateTime	,
-@edad				Tinyint			,
-@Direccion			Varchar(100)	,
-@telefono			Varchar(15)		,
-@ruc_contacto 		Varchar(11)		,
-@Id_Clientes		Int	Output
-as
-	Begin
-		Begin Transaction
-			Insert Into Tb_Cliente_Pasajes(
-				Tipo_Doc_id		,
-				Numero_Doc		,
-				Nombre_Clientes	,
-				Apellido_P		,
-				Apellido_M		,
-				fec_nac			,
-				edad			,
-				Direccion		,
-				telefono		,
-				ruc_contacto 
-			)
-			Values(
-				@Tipo_Doc_Id	 ,
-				@Numero_Doc		 ,
-				@Nombre_Clientes ,
-				@Apellido_P		 ,
-				@Apellido_M		 ,
-				@fec_nac		 ,
-				@edad			 ,
-				@Direccion		 ,
-				@telefono		 ,
-				@ruc_contacto 			
-			)
+ALTER PROCEDURE scwsp_GrabarPasajero (
+	@Tipo_Doc_Id CHAR(1)
+	,@Numero_Doc VARCHAR(15)
+	,@Nombre_Clientes VARCHAR(100)
+	,@Apellido_P VARCHAR(50)
+	,@Apellido_M VARCHAR(50)
+	,@fec_nac SMALLDATETIME
+	,@edad TINYINT
+	,@Direccion VARCHAR(100)
+	,@telefono VARCHAR(15)
+	,@ruc_contacto VARCHAR(11)
+	,@Id_Clientes INT OUTPUT
+	,@sexo CHAR(1)
+)
+AS
+BEGIN
+	BEGIN TRANSACTION
 
-			Set @Id_Clientes=SCOPE_IDENTITY()
+	IF NOT EXISTS (SELECT TOP 1 1 FROM Tb_Cliente_Pasajes WHERE Tipo_Doc_id = @Tipo_Doc_Id AND Numero_Doc = @Numero_Doc)
+	BEGIN
+		INSERT INTO Tb_Cliente_Pasajes (
+		Tipo_Doc_id
+		,Numero_Doc
+		,Nombre_Clientes
+		,Apellido_P
+		,Apellido_M
+		,fec_nac
+		,edad
+		,Direccion
+		,telefono
+		,ruc_contacto
+		,sexo
+		)
+	VALUES (
+		@Tipo_Doc_Id
+		,@Numero_Doc
+		,@Nombre_Clientes
+		,@Apellido_P
+		,@Apellido_M
+		,@fec_nac
+		,@edad
+		,@Direccion
+		,@telefono
+		,@ruc_contacto
+		,@sexo
+		)
+	END;
 
-		If @@ERROR<>0
-			RollBack Transaction
-		Else
-			Commit Transaction
-	End
+	SET @Id_Clientes = SCOPE_IDENTITY()
+
+	IF @@ERROR <> 0
+		ROLLBACK TRANSACTION
+	ELSE
+		COMMIT TRANSACTION
+END
