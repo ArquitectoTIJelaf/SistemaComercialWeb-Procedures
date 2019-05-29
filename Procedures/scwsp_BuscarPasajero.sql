2bd75ae558198209bb3cd85902445b10ecee14c2
777sc <46906763+777sc@users.noreply.github.com>
2019-05-29 17:49:28 -0500

    Update scwsp_BuscarPasajero.sql

--- Procedures/scwsp_BuscarPasajero.sql
+++ Procedures/scwsp_BuscarPasajero.sql
@@ -1,21 +1,35 @@
-Create Procedure scwsp_BuscarPasajero
-@Tipo_Doc_Id		Char(1),
-@Numero_Doc			Varchar(15)
-as
-	Select 
-		Id_Clientes		, 
-		Tipo_Doc_id		,
-		Numero_Doc		,
-		Nombre_Clientes	,
-		Apellido_P		,
-		Apellido_M		,
-		fec_nac			,
-		edad			,
-		Direccion		,
-		telefono		,
-		ruc_contacto	,
-		sexo
-	From Tb_Cliente_Pasajes
-	Where 
-		Tipo_Doc_id=@Tipo_Doc_Id and 
-		Numero_Doc=@Numero_Doc
+ALTER PROCEDURE scwsp_BuscarPasajero -- scwsp_BuscarPasajero '1','78005390'
+@Tipo_Doc_Id CHAR(1)
+,@Numero_Doc VARCHAR(15)
+AS
+
+SELECT
+	Id_Clientes, Tipo_Doc_id, Numero_Doc, Nombre_Clientes, Apellido_P, Apellido_M
+	,fec_nac, edad, Direccion, telefono, ruc_contacto, sexo
+FROM
+	Tb_Cliente_Pasajes
+WHERE
+	Tipo_Doc_id = @Tipo_Doc_Id
+	AND Numero_Doc = @Numero_Doc
+
+UNION
+
+SELECT
+	Id_Clientes, Tipo_Doc_id, Numero_Doc, Nombre_Clientes, Apellido_P, Apellido_M
+	,fec_nac, edad, Direccion, telefono, ruc_contacto, sexo
+FROM
+	Tb_Cliente_Consignado
+WHERE
+	Tipo_Doc_id = @Tipo_Doc_Id
+	AND Numero_Doc = @Numero_Doc
+
+UNION
+
+SELECT
+	Id_Clientes, Tipo_Doc_id, Numero_Doc, Nombre_Clientes, Apellido_P, Apellido_M
+	,fec_nac, edad, Direccion, telefono, ruc_contacto, sexo
+FROM
+	Tb_Cliente_Remitente
+WHERE
+	Tipo_Doc_id = @Tipo_Doc_Id
+	AND Numero_Doc = @Numero_Doc
