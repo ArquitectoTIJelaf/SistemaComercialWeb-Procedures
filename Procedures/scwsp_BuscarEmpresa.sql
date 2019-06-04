57a9b29e4f8af27490f432c35bb17c3e2d8d4f86
777sc <46906763+777sc@users.noreply.github.com>
2019-06-04 12:57:59 -0500

    Update scwsp_BuscarEmpresa.sql

--- Procedures/scwsp_BuscarEmpresa.sql
+++ Procedures/scwsp_BuscarEmpresa.sql
@@ -1,13 +1,27 @@
-Create Procedure scwsp_BuscarEmpresa
-@Ruc_Cliente	Varchar(11)
-as		
-	Begin		
-		Select  
-			Ruc_Cliente		,
-			Razon_Social	,
-			Direccion		,
-			Telefono		
-		From 
-			Tb_Ruc 
-		Where Ruc_Cliente	=	@Ruc_Cliente
-	End
+ALTER PROCEDURE scwsp_BuscarEmpresa
+@Ruc_Cliente VARCHAR(11)
+AS
+
+BEGIN
+	SELECT
+		idruc AS Ruc_Cliente,
+		'' AS Razon_Social,
+		direccion AS Direccion,
+		'' AS Telefono
+	FROM
+		Tb_ruc_Direccion_Fiscal
+	WHERE
+		idruc = @Ruc_Cliente
+
+	UNION ALL
+
+	SELECT
+		Ruc_Cliente
+		,Razon_Social
+		,Direccion
+		,Telefono
+	FROM
+		Tb_Ruc
+	WHERE
+		Ruc_Cliente = @Ruc_Cliente
+END
