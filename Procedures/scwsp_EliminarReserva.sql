39a552b4218aefc6e73a0efe75e26ce394579493
777sc <46906763+777sc@users.noreply.github.com>
2019-06-14 17:08:57 -0500

    Update scwsp_EliminarReserva.sql

--- Procedures/scwsp_EliminarReserva.sql
+++ Procedures/scwsp_EliminarReserva.sql
@@ -2,7 +2,11 @@ Alter procedure scwsp_EliminarReserva
 @IdVenta		Int
 As
   Begin Transaction
-		delete from VENTA where id_venta = @IdVenta;
+
+		delete from VENTA where id_venta = @IdVenta and (FLAG_VENTA = 'R' OR FLAG_VENTA = 'R1');
+
+		delete TB_RESERVACION_HORA_FECHA where id_venta = @IdVenta
+
 		Select @@ROWCOUNT as Validator;
 
  If @@Error<>0
