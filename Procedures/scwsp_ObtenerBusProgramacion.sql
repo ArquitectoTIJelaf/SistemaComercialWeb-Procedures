6a135d1e6a4c1f3fc2d516694fbed43c41b60dfe
hsalazar <hectorsalazarv@outlook.com>
2019-07-02 13:02:35 -0500

    modify scwsp_ObtenerBusProgramacion

--- Procedures/scwsp_ObtenerBusProgramacion.sql
+++ Procedures/scwsp_ObtenerBusProgramacion.sql
@@ -1,12 +1,33 @@
-Alter Procedure scwsp_ObtenerBusProgramacion
-@Codi_Programacion		int
-as
-Begin
-	select p.Codi_bus,b.Plan_bus,b.Nume_Pasajeros,b.Plac_bus,
-	p.Codi_Chofer,ch.nomb_Chofer Nombre_Chofer,p.Codi_Copiloto,
-	co.nomb_Chofer Nombre_Copiloto from Tb_Programacion p with(nolock)
-	Inner Join Tb_Bus b on p.Codi_Bus=b.Codi_Bus
-	Inner Join Tb_Chofer ch on p.Codi_Chofer=ch.Codi_Chofer
-	Inner Join Tb_Chofer co on p.Codi_Copiloto=co.Codi_Chofer
-	Where Codi_Programacion=@Codi_Programacion 
-End
+-- =============================================
+-- Author:		Williams Morales
+-- Create date: 24/01/2019
+-- Description:	Obtiene datos del Bus Programado 
+--				por Código de Programación
+-- =============================================
+-- Author:		Héctor Salazar
+-- Create date: 02/07/2019
+-- Description:	Add Char Activo
+-- =============================================
+ALTER PROCEDURE scwsp_ObtenerBusProgramacion 
+ @Codi_Programacion INT
+AS
+BEGIN
+	SELECT 
+	 p.Codi_bus			AS Codi_bus
+	 ,b.Plan_bus		AS Plan_bus
+	 ,b.Nume_Pasajeros	AS Nume_Pasajeros
+	 ,b.Plac_bus		AS Plac_bus
+	 ,p.Codi_Chofer		AS Codi_Chofer
+	 ,ch.nomb_Chofer	AS Nombre_Chofer
+	 ,p.Codi_Copiloto	AS Codi_Copiloto
+	 ,co.nomb_Chofer	AS Nombre_Copiloto
+	 ,case isnull(p.Activo,'') 
+	   when 1 then '1'
+	   else '' end		AS Activo
+	FROM Tb_Programacion p WITH (NOLOCK)
+	 INNER JOIN Tb_Bus b		ON p.Codi_Bus = b.Codi_Bus
+	 INNER JOIN Tb_Chofer ch	ON p.Codi_Chofer = ch.Codi_Chofer
+	 INNER JOIN Tb_Chofer co	ON p.Codi_Copiloto = co.Codi_Chofer
+	WHERE 
+	 Codi_Programacion = @Codi_Programacion
+END
