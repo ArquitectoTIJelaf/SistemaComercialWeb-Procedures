7112152d06e18b6ffdd2503752a665d67c9d70f6
777sc <46906763+777sc@users.noreply.github.com>
2019-07-11 11:21:12 -0500

    Update scwsp_ListarPuntosVenta.sql

--- Procedures/scwsp_ListarPuntosVenta.sql
+++ Procedures/scwsp_ListarPuntosVenta.sql
@@ -1,8 +1,17 @@
-Create Procedure scwsp_ListarPuntosVenta
-as
-	Select 
-	Codi_Sucursal		,
-	Codi_puntoVenta		,
-	Descripcion			
-	From 
-	Tb_PuntoVenta
+Alter PROCEDURE scwsp_ListarPuntosVenta
+
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	SELECT
+		Codi_puntoVenta,
+		Descripcion,
+		Codi_Sucursal
+	FROM
+		Tb_PuntoVenta
+	ORDER BY
+		Codi_puntoVenta
+
+	SET NOCOUNT OFF
+END
