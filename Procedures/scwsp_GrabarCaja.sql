8f93c460399ea29d9a578265aabf0c32e0550371
777sc <46906763+777sc@users.noreply.github.com>
2019-07-12 20:06:38 -0500

    Update scwsp_GrabarCaja.sql

--- Procedures/scwsp_GrabarCaja.sql
+++ Procedures/scwsp_GrabarCaja.sql
@@ -1,101 +1,113 @@
-Create Procedure scwsp_GrabarCaja  
-@Nume_Caja			char(7),    
-@Codi_Empresa		TinyInt,    
-@Codi_Sucursal		SmallInt,      
-@Boleto				Varchar(15),     
-@Monto				Real, 
-@Codi_Usuario		SmallInt,
-@Recibe				varchar(50),
-@Codi_Destino		varchar(4), 
-@Fecha_Viaje		SmallDateTime,
-@Hora_Viaje			varchar(7),
-@Codi_PuntoVenta	SmallInt,  --Codigo Punto Venta 
-@Id_Venta			Int  ,  --Codigo de Venta
-@Origen				Varchar(2),
-@Modulo				Varchar(2), 
-@Tipo				Varchar(2), --B=Boleta Venta,F=Factura 
-@IdCaja				Int Output
-as   
-	Begin    
-		Begin Transaction
-			Declare @Nom_Uusario		Varchar(30)
-			Declare @Nume_Boleto		Varchar(20)
-			if @Tipo='B' or @Tipo='F'
-				Begin
-					Set @Nume_Boleto=@Tipo+@Boleto
-				End
-			Select @Nom_Uusario=Login From Tb_Usuario
-			Where Codi_Usuario=@Codi_Usuario
-			INSERT INTO CAJA(
-				NUME_CAJA,
-				CODI_EMPRESA,
-				CODI_SUCURSAL,
-				FECH_CAJA,
-				TIPO_VALE,
-				NUME_BOLETO,
-				AUTO_CAJA,
-				CODI_BUS,
-				CODI_CHOFER,
-				CODI_GASTO,
-				CONC_CAJA,
-				MONT_CAJA,
-				CLAV_USUARIO,
-				INDI_ANULADO,
-				TIPO_DESCUENTO,
-				TIPO_DOC,
-				TIPO_GASTO,
-				LIQUI,
-				DIFERENCIA,
-				RECIBE,
-				DESTINO,
-				FECHA_V,
-				HORA_V,
-				PUNTO_VENTA,
-				VOUCHER,
-				ASIENTO,
-				RUC,
-				ID_VENTA,
-				origen,
-				modulo,
-				tipo
-				)    
-			VALUES(
-				@NUME_CAJA,
-				@Codi_Empresa,
-				@Codi_Sucursal,
-				Convert(Varchar(10),getdate(),103),
-				'S',
-				@Boleto,
-				@Nom_Uusario,
-				'',
-				'',
-				'',
-				@Nume_Boleto,
-				@Monto,
-				@Codi_Usuario,
-				'F',
-				'',
-				'XX',
-				'P',
-				0,
-				0,
-				@Recibe,
-				@Codi_Destino,
-				@Fecha_Viaje,
-				@Hora_Viaje,
-				@Codi_PuntoVenta,
-				'PA',
-				'',
-				'N',
-				@Id_Venta,
-				@Origen,
-				@Modulo,
-				@Tipo
-			)    
-		SET @IdCaja= scope_identity()  
+Alter PROCEDURE scwsp_GrabarCaja
+ @Nume_Caja CHAR(7)
+,@Codi_Empresa TINYINT
+,@Codi_Sucursal SMALLINT
+,@FECH_CAJA SMALLDATETIME
+,@Boleto VARCHAR(15)
+,@Monto REAL
+,@Codi_Usuario SMALLINT
+,@Recibe VARCHAR(50)
+,@Codi_Destino VARCHAR(4)
+,@Fecha_Viaje SMALLDATETIME
+,@Hora_Viaje VARCHAR(7)
+,@Codi_PuntoVenta SMALLINT
+,@Id_Venta INT
+,@Origen VARCHAR(2)
+,@Modulo VARCHAR(2)
+,@Tipo VARCHAR(2)
 
-		If @@ERROR<>0
-			RollBack Transaction
-		Else
-			Commit Transaction  
-	End 
+,@Nom_Usario varchar(30)
+,@CONC_CAJA VARCHAR(20)
+,@TIPO_VALE char(1)
+,@CODI_BUS varchar(5)
+,@CODI_CHOFER varchar(5)
+,@CODI_GASTO varchar(3)
+,@INDI_ANULADO char(1)
+,@TIPO_DESCUENTO varchar(5)
+,@TIPO_DOC char(2)
+,@TIPO_GASTO varchar(2)
+,@LIQUI smallmoney
+,@DIFERENCIA smallmoney
+,@VOUCHER varchar(15)
+,@ASIENTO varchar(10)
+,@RUC varchar(11)
+
+,@IdCaja INT OUTPUT
+AS
+BEGIN
+	BEGIN TRANSACTION
+
+	INSERT INTO CAJA (
+		NUME_CAJA
+		,CODI_EMPRESA
+		,CODI_SUCURSAL
+		,FECH_CAJA
+		,TIPO_VALE
+		,NUME_BOLETO
+		,AUTO_CAJA
+		,CODI_BUS
+		,CODI_CHOFER
+		,CODI_GASTO
+		,CONC_CAJA
+		,MONT_CAJA
+		,CLAV_USUARIO
+		,INDI_ANULADO
+		,TIPO_DESCUENTO
+		,TIPO_DOC
+		,TIPO_GASTO
+		,LIQUI
+		,DIFERENCIA
+		,RECIBE
+		,DESTINO
+		,FECHA_V
+		,HORA_V
+		,PUNTO_VENTA
+		,VOUCHER
+		,ASIENTO
+		,RUC
+		,ID_VENTA
+		,origen
+		,modulo
+		,tipo
+	)
+	VALUES (
+		@NUME_CAJA
+		,@Codi_Empresa
+		,@Codi_Sucursal
+		,@FECH_CAJA
+		,@TIPO_VALE
+		,@Boleto
+		,@Nom_Usario
+		,@CODI_BUS
+		,@CODI_CHOFER
+		,@CODI_GASTO
+		,@CONC_CAJA
+		,@Monto
+		,@Codi_Usuario
+		,@INDI_ANULADO
+		,@TIPO_DESCUENTO
+		,@TIPO_DOC
+		,@TIPO_GASTO
+		,@LIQUI
+		,@DIFERENCIA
+		,@Recibe
+		,@Codi_Destino
+		,@Fecha_Viaje
+		,@Hora_Viaje
+		,@Codi_PuntoVenta
+		,@VOUCHER
+		,@ASIENTO
+		,@RUC
+		,@Id_Venta
+		,@Origen
+		,@Modulo
+		,@Tipo
+	);
+
+	SET @IdCaja = scope_identity();
+
+	IF @@ERROR <> 0
+		ROLLBACK TRANSACTION
+	ELSE
+		COMMIT TRANSACTION
+END
