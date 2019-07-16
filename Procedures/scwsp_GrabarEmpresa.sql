05607bc7aed5cddd9b3a0afb1eeb907a8f74ff98
777sc <46906763+777sc@users.noreply.github.com>
2019-07-16 11:09:32 -0500

    Update scwsp_GrabarEmpresa.sql

--- Procedures/scwsp_GrabarEmpresa.sql
+++ Procedures/scwsp_GrabarEmpresa.sql
@@ -1,26 +1,28 @@
-Create Procedure scwsp_GrabarEmpresa
-@Ruc_Cliente	Varchar(11),
-@Razon_Social	Varchar(50),
-@Direccion		Varchar(50),
-@Telefono		Varchar(15)
-as		
-	Begin
-	  Begin Transaction   			
-			INSERT INTO Tb_Ruc(
-				Ruc_Cliente		,
-				Razon_Social	,
-				Direccion		,
-				Telefono
-			)
-			VALUES(
-				@Ruc_Cliente		,
-				Upper(@Razon_Social),
-				@Direccion			,
-				@Telefono			
-			)
+Alter PROCEDURE scwsp_GrabarEmpresa (
+	@Ruc_Cliente VARCHAR(11)
+	,@Razon_Social VARCHAR(100)
+	,@Direccion VARCHAR(200)
+	,@Telefono VARCHAR(15)
+	)
+AS
+BEGIN
+	BEGIN TRANSACTION
 
-		If @@Error<>0
-			RollBack Transaction                
-		Else
-			Commit Transaction
-	End
+	INSERT INTO Tb_Ruc (
+		Ruc_Cliente
+		,Razon_Social
+		,Direccion
+		,Telefono
+		)
+	VALUES (
+		@Ruc_Cliente
+		,Upper(@Razon_Social)
+		,@Direccion
+		,@Telefono
+		)
+
+	IF @@Error <> 0
+		ROLLBACK TRANSACTION
+	ELSE
+		COMMIT TRANSACTION
+END
