6d2cd560403399750ab8bf821e6b81fe2ea167ab
777sc <46906763+777sc@users.noreply.github.com>
2019-08-13 17:52:23 -0500

    Update scwsp_BuscarPasajero.sql

--- Procedures/scwsp_BuscarPasajero.sql
+++ Procedures/scwsp_BuscarPasajero.sql
@@ -1,35 +1,44 @@
 ALTER PROCEDURE scwsp_BuscarPasajero -- scwsp_BuscarPasajero '1','78005390'
-@Tipo_Doc_Id CHAR(1)
-,@Numero_Doc VARCHAR(15)
+@Tipo_Doc_Id CHAR(1),
+@Numero_Doc VARCHAR(15)
 AS
 
-SELECT TOP 1
-	Id_Clientes, Tipo_Doc_id, Numero_Doc, Nombre_Clientes, Apellido_P, Apellido_M
-	,fec_nac, edad, Direccion, telefono, ruc_contacto, sexo
-FROM
-	Tb_Cliente_Pasajes
-WHERE
-	Tipo_Doc_id = @Tipo_Doc_Id
-	AND Numero_Doc = @Numero_Doc
+BEGIN
+	SET NOCOUNT ON;
 
-UNION ALL
+	SELECT TOP 1
+		*
+	FROM (
+		SELECT
+			Id_Clientes, Tipo_Doc_id, Numero_Doc, Nombre_Clientes, Apellido_P, Apellido_M,
+		fec_nac, edad, Direccion, telefono, ruc_contacto, sexo, ESPECIAL
+		FROM
+			Tb_Cliente_Pasajes
+		WHERE
+			Tipo_Doc_id = @Tipo_Doc_Id
+			AND Numero_Doc = @Numero_Doc
 
-SELECT TOP 1
-	Id_Clientes, Tipo_Doc_id, Numero_Doc, Nombre_Clientes, Apellido_P, Apellido_M
-	,fec_nac, edad, Direccion, telefono, ruc_contacto, sexo
-FROM
-	Tb_Cliente_Consignado
-WHERE
-	Tipo_Doc_id = @Tipo_Doc_Id
-	AND Numero_Doc = @Numero_Doc
+		UNION ALL
 
-UNION ALL
+		SELECT
+			Id_Clientes, Tipo_Doc_id, Numero_Doc, Nombre_Clientes, Apellido_P, Apellido_M,
+		fec_nac, edad, Direccion, telefono, ruc_contacto, sexo, ESPECIAL
+		FROM
+			Tb_Cliente_Consignado
+		WHERE
+			Tipo_Doc_id = @Tipo_Doc_Id
+			AND Numero_Doc = @Numero_Doc
 
-SELECT TOP 1
-	Id_Clientes, Tipo_Doc_id, Numero_Doc, Nombre_Clientes, Apellido_P, Apellido_M
-	,fec_nac, edad, Direccion, telefono, ruc_contacto, sexo
-FROM
-	Tb_Cliente_Remitente
-WHERE
-	Tipo_Doc_id = @Tipo_Doc_Id
-	AND Numero_Doc = @Numero_Doc
+		UNION ALL
+
+		SELECT
+			Id_Clientes, Tipo_Doc_id, Numero_Doc, Nombre_Clientes, Apellido_P, Apellido_M,
+		fec_nac, edad, Direccion, telefono, ruc_contacto, sexo, ESPECIAL
+		FROM
+			Tb_Cliente_Remitente
+		WHERE
+			Tipo_Doc_id = @Tipo_Doc_Id
+			AND Numero_Doc = @Numero_Doc
+	) AS SUB;
+
+END;
