407b469d36ee047b17568133628cbe94d7e3b343
777sc <46906763+777sc@users.noreply.github.com>
2019-08-13 16:44:15 -0500

    Update scwsp_ValidarClaveInterna.sql

--- Procedures/scwsp_ValidarClaveInterna.sql
+++ Procedures/scwsp_ValidarClaveInterna.sql
@@ -1,17 +1,23 @@
 ALTER PROCEDURE scwsp_ValidarClaveInterna
-(
-	@Codi_Oficina SMALLINT
-	,@Password VARCHAR(30)
-	,@Codi_Tipo SMALLINT
-)
+@Codi_Oficina SMALLINT,
+@Password VARCHAR(30),
+@Codi_Tipo SMALLINT
+
 AS
-SELECT codigo
-	,oficina
-	,pwd
-	,cod_tipo
-FROM
-	Tb_Claves_Internas
-WHERE
-	Oficina = @Codi_Oficina
-	AND Pwd = @Password
-	AND Cast(Cod_Tipo AS SMALLINT) = @Codi_Tipo
+
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT TOP 1
+		codigo,
+		oficina,
+		pwd,
+		cod_tipo
+	FROM
+		Tb_Claves_Internas
+	WHERE
+		Oficina = @Codi_Oficina
+		AND Pwd = @Password
+		AND Cast(Cod_Tipo AS SMALLINT) = @Codi_Tipo;
+
+END;
