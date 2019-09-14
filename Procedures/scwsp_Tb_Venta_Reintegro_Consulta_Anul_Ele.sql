961ff389b07c2b94b414cbf14fcf88bdc0681a17
777sc <46906763+777sc@users.noreply.github.com>
2019-09-14 08:23:50 -0500

    Update scwsp_Tb_Venta_Reintegro_Consulta_Anul_Ele.sql

--- Procedures/scwsp_Tb_Venta_Reintegro_Consulta_Anul_Ele.sql
+++ Procedures/scwsp_Tb_Venta_Reintegro_Consulta_Anul_Ele.sql
@@ -1,15 +1,15 @@
-ALTER proc scwsp_Tb_Venta_Reintegro_Consulta_Anul_Ele -- scwsp_Tb_Venta_Reintegro_Consulta_Anul_Ele '854','0000487', 1, 'B'
+Alter proc scwsp_Tb_Venta_Reintegro_Consulta_Anul_Ele -- scwsp_Tb_Venta_Reintegro_Consulta_Anul_Ele '326','0000663', 1, 'F'
 @ser  varchar(3),
 @bol  varchar(7),
 @emp  varchar(2),
 @tipo varchar(1)
 As
 Begin
-	Set nocount on 
+	Set nocount on
 
 	Select
-		id_venta, codi_esca, Codi_Sucursal, PREC_VENTA, tipo_pago, clav_usuario, tipo, NIT_CLIENTE, FECH_VENTA, CODI_EMPRESA, 
-		SUC_VENTA, NOMBRE, TIPO_DOC, DNI, EDAD, NIT_CLIENTE, TELEFONO
+		id_venta, codi_esca, Codi_Sucursal, PREC_VENTA, tipo_pago, clav_usuario, tipo, NIT_CLIENTE, FECH_VENTA, CODI_EMPRESA,
+		SUC_VENTA, NOMBRE, TIPO_DOC, DNI, EDAD, TELEFONO
 	From
 		VENTA
 	Where
@@ -18,5 +18,5 @@ Begin
 		and tipo = @tipo
 		and codi_Empresa = @emp
 		and (flag_venta = 'O' or flag_venta = '1')
-		and indi_anulado = 'F'       
+		and indi_anulado = 'F'
 End;
