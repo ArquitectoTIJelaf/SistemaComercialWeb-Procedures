17f337e83ed7bf5f2595b85575edf653284d9fb2
777sc <46906763+777sc@users.noreply.github.com>
2019-09-23 10:59:22 -0500

    Update scwsp_BuscarVentaxBoleto.sql

--- Procedures/scwsp_BuscarVentaxBoleto.sql
+++ Procedures/scwsp_BuscarVentaxBoleto.sql
@@ -3,15 +3,53 @@ Alter Procedure scwsp_BuscarVentaxBoleto
 @Serie_Boleto		SmallInt,
 @Nume_Boleto		Int,
 @Codi_Empresa		TinyInt
-as
-	Select v.Id_Venta, v.NOMBRE,v.cod_Origen Codi_Origen, ori.Descripcion Nom_Origen,
-	v.CODI_SUBRUTA Codi_Destino, des.Descripcion Nom_Destino,
-	ser.descripcion Nom_Servicio,vd.Fecha_Viaje,vd.Hora_Viaje,
-	v.NUME_ASIENTO From Venta v
-	Inner Join VENTA_DERIVADA vd on v.id_venta=vd.id_venta
-	Inner Join Tb_Oficinas ori on v.cod_origen=ori.Codi_Sucursal
-	Inner Join Tb_Oficinas des on v.CODI_SUBRUTA=des.Codi_Sucursal
-	Inner Join Tb_Servicio ser on vd.Servicio=ser.Codi_Servicio
-	Where v.SERIE_BOLETO=@Serie_Boleto and v.NUME_BOLETO=@Nume_Boleto and 
-	v.tipo=@Tipo and v.CODI_EMPRESA=@Codi_Empresa and v.INDI_ANULADO='F'
-	and v.FLAG_VENTA<>'X' and v.FLAG_VENTA<>'R'
+As
+
+Set nocount on;
+
+Begin
+
+	--DECLARE @Tipo				Varchar(1)	= 'F';
+	--DECLARE @Serie_Boleto		SmallInt	= 505;
+	--DECLARE @Nume_Boleto		Int			= 532;
+	--DECLARE @Codi_Empresa		TinyInt		= 1;
+
+	Select Top 1
+	v.Id_Venta,
+	v.NOMBRE,
+	v.cod_Origen Codi_Origen,
+	v.CODI_SUBRUTA Codi_Destino,
+	vd.Fecha_Viaje,
+	vd.Hora_Viaje,
+	v.NUME_ASIENTO,
+	vd.Servicio as Codi_Servicio,
+	v.CODI_PROGRAMACION,
+	v.Punto_Venta Codi_PuntoVenta,
+	p.Fech_programacion,
+	p.Codi_Servicio as Codi_Servicio_programacion,
+	v.FLAG_VENTA,
+	v.TIPO_DOC,
+	v.DNI,
+	v.imp_manifiesto,
+	ISNULL(p.Activo, '') as Cierre,
+	an.Nivel as NivelAsiento,
+	P.Codi_ruta,
+	v.PREC_VENTA
+	From 
+		Venta v
+		Inner Join VENTA_DERIVADA vd
+			on v.id_venta = vd.id_venta
+		Left Join Tb_Programacion p
+			on v.CODI_PROGRAMACION = p.Codi_Programacion
+		Left Join tb_asientonivel an
+			on p.Codi_Bus = an.Codi_Bus
+			and v.NUME_ASIENTO = an.Nume_Asiento
+	Where
+		v.tipo = @Tipo
+		and v.SERIE_BOLETO = @Serie_Boleto
+		and v.NUME_BOLETO = @Nume_Boleto
+		and v.CODI_EMPRESA = @Codi_Empresa
+		and v.CODI_PROGRAMACION > 0
+		and v.INDI_ANULADO = 'F'
+		and v.FLAG_VENTA not in ('X', 'R', 'O');
+End;
