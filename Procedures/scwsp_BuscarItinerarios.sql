3f7270d3ae5ca32c6b58819fae5180108e0bddfa
777sc <46906763+777sc@users.noreply.github.com>
2019-09-23 11:09:27 -0500

    Update scwsp_BuscarItinerarios.sql

--- Procedures/scwsp_BuscarItinerarios.sql
+++ Procedures/scwsp_BuscarItinerarios.sql
@@ -1,26 +1,49 @@
-Alter PROCEDURE scwsp_BuscarItinerarios
+SET QUOTED_IDENTIFIER OFF
+GO
+
+ALTER PROCEDURE scwsp_BuscarItinerarios
 @Codi_Origen	SmallInt,
 @Codi_Destino	SmallInt,
 @Codi_Ruta		SmallInt,
-@Hora			varchar(7)
+@Hora			varchar(7),
+@Codi_Servicio	SmallInt
 AS
+
 SET NOCOUNT ON
 BEGIN
+	
+	--SET QUOTED_IDENTIFIER OFF
+	--GO
 
 	--DECLARE @Codi_Origen	SmallInt	= 6;
-	--DECLARE @Codi_Destino	SmallInt	= 2;
-	--DECLARE @Codi_Ruta		SmallInt	= 2;
+
+	--DECLARE @Codi_Destino	SmallInt	= 25;
+	--DECLARE @Codi_Ruta		SmallInt	= 0;
 	--DECLARE @Hora			varchar(7)	= '';
+	--DECLARE @Codi_Servicio	SmallInt	= 0;
 
 	DECLARE @Tiempo Int
 	SET @Tiempo = 10
 
 	IF @Hora <> '00:00AM'
-		BEGIN
-			SET @Tiempo = -10
-		END
+	BEGIN
+		SET @Tiempo = -10
+	END;
 
-	SELECT TOP 200 * FROM (
+
+	DECLARE @DynamicQuery NVARCHAR(MAX);
+
+	SET @DynamicQuery = "
+	SELECT " +
+		CASE
+			WHEN (@Codi_Destino = 0 OR @Codi_Ruta = 0) AND @Hora = '' THEN
+				""
+			ELSE
+				"TOP 150"
+		END
+		+ "
+		*
+	FROM (
 		SELECT DISTINCT
 			Tb_Maestro_Programacion.NRO_VIAJE,
 			Tb_Maestro_Programacion.CODI_EMPRESA,
@@ -30,6 +53,7 @@ BEGIN
 			suc.Descripcion AS Nom_Sucursal,
 			Tb_Ruta_Maestro.CODI_DESTINO AS Codi_Ruta,
 			rut.Descripcion AS Nom_Ruta,
+
 			Tb_Ruta_Maestro.CODI_SERVICIO,
 			Tb_Servicio.descripcion AS Nom_Servicio,
 			Tb_Maestro_Programacion.CODI_SUCURSAL AS Codi_PuntoVenta,
@@ -45,42 +69,73 @@ BEGIN
 			Tb_Ruta_Intermedio.DIAS
 		FROM
 			Tb_Ruta_Maestro WITH(nolock)
-			INNER JOIN Tb_Maestro_Programacion WITH(nolock) ON Tb_Ruta_Maestro.NRO_RUTA = Tb_Maestro_Programacion.NRO_RUTA
-			INNER JOIN Tb_Ruta_Intermedio WITH(nolock) ON Tb_Maestro_Programacion.NRO_VIAJE = Tb_Ruta_Intermedio.NRO_VIAJE
-	INNER JOIN Tb_Ruta_Intermedio AS dst WITH(nolock) ON Tb_Maestro_Programacion.NRO_VIAJE = dst.NRO_VIAJE
-			INNER JOIN Tb_Empresa ON Tb_Maestro_Programacion.CODI_EMPRESA = Tb_Empresa.Codi_Empresa
-			INNER JOIN Tb_Servicio ON Tb_Ruta_Maestro.CODI_SERVICIO = Tb_Servicio.Codi_Servicio
-			INNER JOIN Tb_Oficinas AS suc ON Tb_Ruta_Maestro.CODI_SUCURSAL = suc.Codi_Sucursal
-			INNER JOIN Tb_Oficinas AS rut ON Tb_Ruta_Maestro.CODI_DESTINO = rut.Codi_Sucursal
-			INNER JOIN Tb_Oficinas AS ori ON Tb_Ruta_Intermedio.CODI_SUCURSAL = ori.Codi_Sucursal
-			INNER JOIN Tb_PuntoVenta AS pv ON pv.Codi_puntoVenta = Tb_Maestro_Programacion.CODI_SUCURSAL
-			AND Tb_Maestro_Programacion.TIPO_E = '1'
-			AND Tb_Ruta_Intermedio.VER = '1'
-			AND (Tb_Ruta_Maestro.CODI_DESTINO = @Codi_Ruta OR @Codi_Ruta = 0)
-			AND (Tb_Ruta_Maestro.CODI_DESTINO <> @Codi_Origen)
-			AND (Tb_Ruta_Maestro.CODI_DESTINO <> 79)
-			AND (Tb_Ruta_Intermedio.CODI_SUCURSAL = @Codi_Origen OR @Codi_Origen = 0)
-	AND (dst.CODI_SUCURSAL = @Codi_Destino OR @Codi_Destino = 0)
-			AND CONVERT(DATETIME, Tb_Ruta_Intermedio.HORA_PASO, 300) >= CONVERT(DATETIME, DATEADD(mi, @Tiempo, @Hora), 300)
-			AND Tb_Maestro_Programacion.TR = 0 -- Gerardo
-			AND Tb_Maestro_Programacion.TIPO_B = 1 -- Gerardo
+			INNER JOIN Tb_Maestro_Programacion WITH(nolock)		ON Tb_Ruta_Maestro.NRO_RUTA = Tb_Maestro_Programacion.NRO_RUTA
+			INNER JOIN Tb_Ruta_Intermedio WITH(nolock)			ON Tb_Maestro_Programacion.NRO_VIAJE = Tb_Ruta_Intermedio.NRO_VIAJE
+			INNER JOIN Tb_Ruta_Intermedio AS dst WITH(nolock)	ON Tb_Maestro_Programacion.NRO_VIAJE = dst.NRO_VIAJE
+			INNER JOIN Tb_Empresa								ON Tb_Maestro_Programacion.CODI_EMPRESA = Tb_Empresa.Codi_Empresa
+			INNER JOIN Tb_Servicio								ON Tb_Ruta_Maestro.CODI_SERVICIO = Tb_Servicio.Codi_Servicio
+			INNER JOIN Tb_Oficinas AS suc						ON Tb_Ruta_Maestro.CODI_SUCURSAL = suc.Codi_Sucursal
+			INNER JOIN Tb_Oficinas AS rut						ON Tb_Ruta_Maestro.CODI_DESTINO = rut.Codi_Sucursal
+			INNER JOIN Tb_Oficinas AS ori						ON Tb_Ruta_Intermedio.CODI_SUCURSAL = ori.Codi_Sucursal
+			INNER JOIN Tb_PuntoVenta AS pv						ON Tb_Maestro_Programacion.CODI_SUCURSAL = pv.Codi_puntoVenta
+				AND Tb_Maestro_Programacion.TIPO_E = '1'
+				AND Tb_Ruta_Intermedio.VER = '1'
+				" +
+				CASE
+					WHEN @Codi_Ruta != 0 THEN
+						"AND Tb_Ruta_Maestro.CODI_DESTINO = " + LTRIM(@Codi_Ruta)
+					ELSE
+						""
+				END
+				+ "
+				AND Tb_Ruta_Maestro.CODI_DESTINO <> " + LTRIM(@Codi_Origen) + "
+				" +
+				CASE
+					WHEN @Codi_Servicio != 0 THEN
+						"AND Tb_Ruta_Maestro.CODI_SERVICIO = " + LTRIM(@Codi_Servicio)
+					ELSE
+						""
+				END
+				+ "
+				" +
+				CASE
+					WHEN @Codi_Origen != 0 THEN
+						"AND Tb_Ruta_Intermedio.CODI_SUCURSAL = " + LTRIM(@Codi_Origen)
+					ELSE
+
+
+						""
+				END
+				+ "
+				" +
+				CASE
+					WHEN @Codi_Destino != 0 THEN
+						"AND dst.CODI_SUCURSAL = " + LTRIM(@Codi_Destino)
+					ELSE
+						""
+				END
+				+ "
+				AND CONVERT(DATETIME, Tb_Ruta_Intermedio.HORA_PASO, 300) >= CONVERT(DATETIME, DATEADD(mi, " + LTRIM(@Tiempo) + ", '" + @Hora + "'), 300)
+				AND Tb_Maestro_Programacion.TR != 1 -- Gerardo
+				AND Tb_Maestro_Programacion.TIPO_B = 1 -- Gerardo
 	) SUBCONSULTA
 	ORDER BY
-			DATEPART(HOUR, SUBCONSULTA.Hora_Partida),
-			DATEPART(MINUTE, SUBCONSULTA.Hora_Partida),
-			SUBCONSULTA.NRO_VIAJE,
-			SUBCONSULTA.CODI_EMPRESA,
-			SUBCONSULTA.NRO_RUTA,
-			SUBCONSULTA.CODI_SUCURSAL,
-			SUBCONSULTA.Codi_Ruta,
-			SUBCONSULTA.CODI_SERVICIO,
-			SUBCONSULTA.Codi_PuntoVenta,
-			SUBCONSULTA.st_opcional,
-			SUBCONSULTA.Codi_Origen,
-			SUBCONSULTA.Hora_Partida,
-			SUBCONSULTA.NRO_RUTA_INT,
-			SUBCONSULTA.DIAS
+		DATEPART(HOUR, SUBCONSULTA.Hora_Partida),
+		DATEPART(MINUTE, SUBCONSULTA.Hora_Partida),
+		SUBCONSULTA.NRO_VIAJE,
+		SUBCONSULTA.CODI_EMPRESA,
+		SUBCONSULTA.NRO_RUTA,
+		SUBCONSULTA.CODI_SUCURSAL,
+		SUBCONSULTA.Codi_Ruta,
+		SUBCONSULTA.CODI_SERVICIO,
+		SUBCONSULTA.Codi_PuntoVenta,
+		SUBCONSULTA.st_opcional,
+		SUBCONSULTA.Codi_Origen,
+		SUBCONSULTA.Hora_Partida,
+		SUBCONSULTA.NRO_RUTA_INT,
+		SUBCONSULTA.DIAS;";
 
-			--DESC OFFSET 0 ROWS -- Gerardo
-			--FETCH NEXT 200 ROWS ONLY -- Gerardo
-END
+	EXECUTE sp_executesql @DynamicQuery;
+	--SELECT @DynamicQuery;
+
+END;
