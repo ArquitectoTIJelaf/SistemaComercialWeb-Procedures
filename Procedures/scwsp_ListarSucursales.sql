f8cd8d95d1f80a8bf7a4e1868c3a11ca8b5dffd5
777sc <46906763+777sc@users.noreply.github.com>
2019-09-23 11:12:52 -0500

    Update scwsp_ListarSucursales.sql

--- Procedures/scwsp_ListarSucursales.sql
+++ Procedures/scwsp_ListarSucursales.sql
@@ -1,17 +1,19 @@
 ALTER PROCEDURE scwsp_ListarSucursales
 AS
-BEGIN
-	SET NOCOUNT ON
 
-		SELECT
-			Codi_Sucursal
-			,Descripcion
-		FROM
-			Tb_Oficinas
-		WHERE
-			Sucursal = 'S'
-		ORDER BY
-			Codi_Sucursal
+SET NOCOUNT ON;
+
+BEGIN
+	
+	SELECT
+		Codi_Sucursal
+		,Descripcion
+	FROM
+		Tb_Oficinas
+	WHERE
+		Sucursal = 'S'
+	ORDER BY
+		Codi_Sucursal
 
 	SET NOCOUNT OFF
 END
